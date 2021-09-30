0ba239f92214e2e60ced57fcbd6c5927713c7d06
simonlindstedt <simon.lindstedt@gmail.com>
2021-09-30 13:21:46 +0200

    nav update

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -5,6 +5,9 @@ window.onload = () => {
   subMenus.forEach((subMenu) => {
     subMenu.querySelector("a").addEventListener("click", (event) => {
       event.preventDefault();
+      if (subMenu.classList.contains("active")) {
+        window.location.href = subMenu.querySelector("a").href;
+      }
       subMenu.classList.toggle("active");
     });
   });
