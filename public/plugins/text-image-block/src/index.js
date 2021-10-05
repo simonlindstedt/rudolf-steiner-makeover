84657f386d0de80cb31411e775eb17485c271f7d
simonlindstedt <simon.lindstedt@gmail.com>
2021-10-05 18:01:50 +0200

    text-image-block wip

---
+++ public/plugins/text-image-block/src/index.js
@@ -0,0 +1,129 @@
+import { registerBlockType } from "@wordpress/blocks";
+import {
+	InspectorControls,
+	MediaUpload,
+	MediaUploadCheck,
+	RichText,
+	useBlockProps,
+} from "@wordpress/block-editor";
+import "./editor.scss";
+import { PanelBody, SelectControl } from "@wordpress/components";
+
+registerBlockType("create-block/text-image-block", {
+	title: "Text Image Block",
+	icon: "media-text",
+	category: "common",
+
+	attributes: {
+		imageUrl: {
+			type: "string",
+			default: "",
+		},
+		imageId: {
+			type: "number",
+			default: 0,
+		},
+		heading: {
+			type: "string",
+			attribute: "h3",
+		},
+		body: {
+			type: "string",
+			attribute: "p",
+		},
+		displayType: {
+			type: "string",
+			default: "spotlight",
+		},
+	},
+
+	edit: ({ attributes, setAttributes }) => {
+		// Attributes
+		const { imageUrl, imageId, heading, body, displayType } = attributes;
+
+		// Functions
+
+		const setHeading = (value) => {
+			setAttributes({ heading: value });
+		};
+		const setBody = (value) => {
+			setAttributes({ body: value });
+		};
+		const setImage = (value) => {
+			setAttributes({ imageUrl: value.url, imageId: value.id });
+		};
+		const setDisplayType = (value) => {
+			setAttributes({ displayType: value });
+		};
+
+		const uploadButton = (open) => {
+			if (imageUrl) {
+				return (
+					<div>
+						<img src={imageUrl} />
+						<button onClick={open}>Choose image</button>
+					</div>
+				);
+			}
+			return <button onClick={open}>Choose image</button>;
+		};
+
+		return [
+			<InspectorControls>
+				<PanelBody title="Type">
+					<p>Select type</p>
+					<select
+						value={displayType}
+						onChange={(e) => {
+							setDisplayType(e.target.value);
+						}}
+					>
+						<option value="spotlight">Spotlight</option>
+						<option value="left">Left</option>
+						<option value="right">Right</option>
+					</select>
+				</PanelBody>
+			</InspectorControls>,
+			<div {...useBlockProps()}>
+				<MediaUploadCheck>
+					<MediaUpload
+						onSelect={setImage}
+						value={imageId}
+						render={({ open }) => uploadButton(open)}
+					/>
+				</MediaUploadCheck>
+				<RichText
+					value={heading}
+					placeholder="heading"
+					onChange={setHeading}
+					style={{
+						fontSize: "24px",
+						backgroundColor: "unset",
+						width: "auto",
+					}}
+				/>
+				<RichText
+					value={body}
+					placeholder="body"
+					onChange={setBody}
+					style={{
+						fontSize: "16px",
+						backgroundColor: "unset",
+						width: "auto",
+					}}
+				/>
+			</div>,
+		];
+	},
+	save: ({ attributes }) => {
+		const { imageUrl, heading, body, displayType } = attributes;
+
+		return (
+			<div className={`text-image-block ${displayType}`}>
+				{imageUrl && <img src={imageUrl} />}
+				<h3>{heading}</h3>
+				<p>{body}</p>
+			</div>
+		);
+	},
+});
