983f1243bdd7d2930bfb96fb50692f1631f60578
Gilda Eklof <gilda@eklof.se>
2021-10-12 15:12:00 +0200

    change color

--- public/plugins/helpers/colors.js
+++ public/plugins/helpers/colors.js
@@ -2,7 +2,7 @@ export default [
   { color: "#F3F7F6", name: "Main Mint White" },
   { color: "#6BA783", name: "Main Green" },
   { color: "#8DC5C9", name: "Main Blue" },
-  { color: "#35524A", name: "Main Dark Sea Green" },
+  { color: "#25342e", name: "Main Dark Sea Green" },
   { color: "#cabc76", name: "Main Yellow" },
   { color: "#b79bbc", name: "Main Lavender" },
   { color: "#ffffff", name: "Full White" },
