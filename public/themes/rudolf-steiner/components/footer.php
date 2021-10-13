850599a797eadb67f5e4b696a432a940e6d40b76
Gilda Eklof <gilda@eklof.se>
2021-10-13 10:07:00 +0200

    some fixes

--- public/themes/rudolf-steiner/components/footer.php
+++ public/themes/rudolf-steiner/components/footer.php
@@ -15,7 +15,7 @@
             </li>
             <li class="top"><img class="icon" src="<?= get_template_directory_uri(); ?>/assets/svg/phone_iphone.svg" />031-21 46 32</li>
             <li class="top"><img class="icon" src="<?= get_template_directory_uri(); ?>/assets/svg/mail_outline.svg" /><a href="mailto:rudolf@steinerskolan.se">rudolf@steinerskolan.se</a></li>
-            <li class="hide"><a href="https://www.instagram.com/rudolfsteinerskolangoteborg/"><img class="icon insta" src="<?= get_template_directory_uri(); ?>/assets/svg/ig.svg" />Vi finns även på Instagram!</a></li>
+            <li class="hide"><a href="https://www.instagram.com/rudolfsteinerskolangoteborg/"><img class="icon insta" src="<?= get_template_directory_uri(); ?>/assets/svg/ig.svg" />Vi finns även på Instagram</a></li>
         </ul>
     </div>
     <div class="column-3">
